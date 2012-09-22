e72fe0d91bfeab1aae84bc30b922c46bd324d4fb
Richard Weiss <richardweiss@richardweiss.org>
2012-09-22 15:47:59 +1000

    Renaming files

--- Data.py
+++ Data.py
@@ -1,6 +1,8 @@
 import csv
 
 class Data(object):
+   """A data point. The string can be accessed through Data.string and the
+   truthfulness through Data.isTrue"""
    def __init__(self, string, isTrue):
       self.string = string
       self.isTrue = isTrue
@@ -11,12 +13,15 @@ class Data(object):
          return "Lie: \"%s\"" % self.string
 
 class DataSet(list):
+   """DataSets extend list, so access them in the obvious way"""
    def __init__(self, items = []):
       for item in items:
          assert(isinstance(item, Data))
       super(DataSet, self).__init__(items)
 
 class FilesDataSet(DataSet):
+   """A FileDataSet is a data set that is built from a list of CSV
+   files. These must be file objects"""
    def __init__(self, files):
       items = []
       for f in files:
--- WorkTypeQuantities.py
+++ WorkTypeQuantities.py
@@ -2,13 +2,15 @@ from Feature import ContinuousFeature
 from Data import Data
 import nltk
 
-
-class StringStructure(ContinuousFeature):
+class SummingScentenceTag(ContinuousFeature):
    def tag(self, data):
+      """Break the string in data down into 
+      tokens and then tag them with their 
+      gramatical type"""
       tokens = nltk.word_tokenize(data)
       tagged = nltk.pos_tag(tokens)
       return nltk.chunk.ne_chunk(tagged)
-   def stepSum(self, itemToCountFn, data):
+   def totalBy(self, itemToCountFn, data):
       stepSum = 0
       tagged = self.tag(data.string)
       for w in tagged:
@@ -16,15 +18,14 @@ class StringStructure(ContinuousFeature):
          if len(w) == 2:
             stepSum += itemToCountFn(w[1])
       return stepSum
-
-class VerbQuantity(StringStructure):
    def build(self, data):
       assert(isinstance(data, Data))
-      return self.stepSum(verbSum, data)
+      return self.totalBy(self.sumFn, data)
 
-def verbSum(v):
-   if "VB" in v:
-      return 1
-   else:
-      return 0
+class VerbQuantity(SummingScentenceTag):
+   def sumFn(self, v):
+      if "VB" in v:
+         return 1
+      else:
+         return 0
 
