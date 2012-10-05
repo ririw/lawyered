28e52896abfc564b56ad2ed85d617fb7bcb9fa23
David George <david@david-laptop.(none)>
2012-10-05 13:00:04 +1000

    Added features as well as some tests.

--- WordTypeQuantities.py
+++ WordTypeQuantities.py
@@ -1,6 +1,7 @@
 from Feature import ContinuousFeature
 from Data import Data
 import nltk
+import string
 
 class SummingScentenceTag(ContinuousFeature):
    def tag(self, data):
@@ -15,7 +16,7 @@ class SummingScentenceTag(ContinuousFeature):
       tagged = self.tag(data.string)
       for w in tagged:
          if len(w) == 2:
-            stepSum += itemToCountFn(w[1])
+            stepSum += itemToCountFn(w)
       return stepSum
    def build(self, data):
       assert(isinstance(data, Data))
@@ -23,8 +24,40 @@ class SummingScentenceTag(ContinuousFeature):
 
 class VerbQuantity(SummingScentenceTag):
    def sumFn(self, v):
-      if "VB" in v:
+      if "VB" in v[1]:
          return 1
       else:
          return 0
 
+class ModifierCheck(SummingScentenceTag):
+    def sumFn(self,v):
+        tags = {"JJ","JJR","JJS","JJT","NR","RB","RBR","RBT","RN","RP","WRB"}
+        if v[1] in tags:
+            return 0
+        else:
+            return 1
+
+class WordQuantity(SummingScentenceTag):
+    def sumFn(self,v):
+        if v[0] in string.punctuation:
+            return 0    
+        else:
+            return 1
+
+class CausationCheck(SummingScentenceTag):
+    def sumFn(self, v):
+        f = open('Causation.txt')
+        causationWords = f.read().split('\n')
+        if v[0].lower() in causationWords:
+            return 1
+        else:
+            return 0
+
+class TentativeCheck(SummingScentenceTag):
+    def sumFn(self, v):
+        f = open('Tentative.txt')
+        tentativeWords = f.read().split('\n')
+        if v[0].lower() in tentativeWords:
+            return 1
+        else:
+            return 0
